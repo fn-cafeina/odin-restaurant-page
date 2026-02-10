3525e32944af34176c4a2d2a133a45784d263279
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-02-10 13:25:41 -0600

    Add menu

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 
 import header from "./js/header.js";
 import home from "./js/home.js";
+import menu from "./js/menu.js";
 
 const body = document.body;
 body.appendChild(header);
@@ -9,6 +10,6 @@ body.appendChild(header);
 const content = document.createElement("div");
 content.id = "content";
 
-content.appendChild(home);
+content.appendChild(menu);
 
 body.appendChild(content);
---
+++ src/js/menu.js
@@ -0,0 +1,40 @@
+import AmericanoImage from "../images/americano.avif";
+import CappuccinoImage from "../images/cappuccino.avif";
+import EspressoImage from "../images/espresso.avif";
+
+const menu = document.createElement("div");
+menu.classList.add("menu");
+
+const hero = document.createElement("p");
+hero.textContent = "Best Selling Item";
+
+const description = document.createElement("p");
+description.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor sapien nibh, sed molestie est suscipit sit amet. Fusce at.`;
+
+const textContainer = document.createElement("div");
+textContainer.appendChild(hero);
+textContainer.appendChild(description);
+
+const cards = document.createElement("div");
+const images = [AmericanoImage, CappuccinoImage, EspressoImage];
+const names = ["Americano", "Cappuccino", "Espresso"];
+for (let i = 0; i <= 3; i++) {
+  const card = document.createElement("div");
+  const name = document.createElement("p");
+  const image = new Image();
+
+  name.textContent = names[i];
+  image.src = images[i];
+  image.width = "200";
+  image.height = "auto";
+
+  card.appendChild(name);
+  card.appendChild(image);
+
+  cards.appendChild(card);
+}
+
+menu.appendChild(textContainer);
+menu.appendChild(cards);
+
+export default menu;
